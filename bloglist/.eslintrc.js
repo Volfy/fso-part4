f94fa3f7729c2b78ddbbc0fe859dd600b38ecfdd
sasha v <volfym79@gmail.com>
2023-01-27 16:48:16 +0100

    exercise 4.1

---
+++ bloglist/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'linebreak-style': 0,
+    semi: [
+      'error',
+      'never',
+    ],
+    'no-console': 0,
+  },
+}
