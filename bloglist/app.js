4ce69a1a10fc5287476d7c053027ee49fd016cb8
sasha v <volfym79@gmail.com>
2023-01-31 17:09:33 +0100

    refactor config logger app blog

---
+++ bloglist/app.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then((blogs) => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = app
