1e3040df8938c8a9c240df0730bf3790c4bd0f75
sasha v <volfym79@gmail.com>
2023-02-01 09:32:20 +0100

    exercise 4.4

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,6 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item.likes
+
+  return blogs
+    ? blogs.reduce(reducer, 0)
+    : 0
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
