a393342a822442a2cd745bb70ecc530feeb4255e
sasha v <volfym79@gmail.com>
2023-02-01 10:24:47 +0100

    exercise 4.5*

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -4,12 +4,24 @@ const dummy = (blogs) => 1
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => sum + item.likes
 
-  return blogs
-    ? blogs.reduce(reducer, 0)
-    : 0
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const maxLikes = blogs.reduce((sum, item) => (sum > item.likes ? sum : item.likes), 0)
+  const faves = blogs.find((b) => b.likes === maxLikes)
+
+  return {
+    title: faves.title || faves[0].title,
+    author: faves.author || faves[0].author,
+    url: faves.url || faves[0].url,
+  }
 }
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
