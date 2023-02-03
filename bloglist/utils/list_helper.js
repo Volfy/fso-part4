638bcd779f0ab97ae6764f9ee03b91b31e5d46d1
sasha v <volfym79@gmail.com>
2023-02-03 23:35:14 +0100

    comments in list_helper

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -27,6 +27,8 @@ const mostBlogs = (blogs) => {
 
   const authors = _.countBy(blogs, 'author')
 
+  // rearrange authors list to include author as property
+  // find the maximum by blogs
   return _.maxBy(_.map(
     Object.keys(authors),
     (val) => ({ author: val, blogs: authors[val] }),
@@ -38,6 +40,10 @@ const mostLikes = (blogs) => {
   if (blogs.length === 0) return null
 
   const grouped = _.groupBy(blogs, 'author')
+
+  // calculate total likes for each author
+  // rearrange to include author as property
+  // find the maximum by likes
   return _.maxBy(_.map(
     Object.keys(grouped),
     (val) => ({ author: val, likes: totalLikes(grouped[val]) }),
