f5a2e323743fd1499dfd81088d9657c5709a1a2a
sasha v <volfym79@gmail.com>
2023-02-06 11:08:30 +0100

    refactor

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -14,84 +14,88 @@ beforeEach(async () => {
   await Promise.all(promiseArr)
 })
 
-test('all blogs are returned', async () => {
-  const res = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
+describe('when there are already some blogs', () => {
+  test('all blogs are returned', async () => {
+    const res = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blogs have unique identifier property named id', async () => {
+    const res = await api
+      .get('/api/blogs')
+    expect(res.body[0].id).toBeDefined()
+  })
 })
 
-test('blogs have unique identifier property named id', async () => {
-  const res = await api
-    .get('/api/blogs')
-  expect(res.body[0].id).toBeDefined()
-})
+describe('addition of a new note', () => {
+  test('valid blog can be added', async () => {
+    const newBlog = {
+      title: 'Dumb sentence for a title',
+      author: 'John Doe',
+      url: 'xyz.com',
+      likes: 0,
+    }
 
-test('valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Dumb sentence for a title',
-    author: 'John Doe',
-    url: 'xyz.com',
-    likes: 0,
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const res = await api.get('/api/blogs')
 
-  const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(res.body.map((r) => r.title)).toContain('Dumb sentence for a title')
+  })
 
-  expect(res.body).toHaveLength(helper.initialBlogs.length + 1)
-  expect(res.body.map((r) => r.title)).toContain('Dumb sentence for a title')
-})
+  test('likes default to 0 if not provided', async () => {
+    const newBlog = {
+      title: 'Dumb sentence for a title',
+      author: 'John Doe',
+      url: 'xyz.com',
+    }
 
-test('likes default to 0 if not provided', async () => {
-  const newBlog = {
-    title: 'Dumb sentence for a title',
-    author: 'John Doe',
-    url: 'xyz.com',
-  }
+    const res = await api
+      .post('/api/blogs')
+      .send(newBlog)
 
-  const res = await api
-    .post('/api/blogs')
-    .send(newBlog)
+    expect(res.body.likes).toEqual(0)
+  })
 
-  expect(res.body.likes).toEqual(0)
-})
+  test('fails to post w/ code 400 if title is missing', async () => {
+    const newBlog = {
+      author: 'John Doe',
+      url: 'xyz.com',
+      likes: 0,
+    }
 
-test('fails to post if title is missing', async () => {
-  const newBlog = {
-    author: 'John Doe',
-    url: 'xyz.com',
-    likes: 0,
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+  })
 
-  const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
-})
+  test('fails to post w/ code 400 if url is missing', async () => {
+    const newBlog = {
+      title: 'Dumb title',
+      author: 'John Doe',
+      likes: 0,
+    }
 
-test('fails to post if url is missing', async () => {
-  const newBlog = {
-    title: 'Dumb title',
-    author: 'John Doe',
-    likes: 0,
-  }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const res = await api.get('/api/blogs')
-  expect(res.body).toHaveLength(helper.initialBlogs.length)
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(async () => {
